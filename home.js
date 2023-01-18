dc0c3d54ec92008e4346c4efd7c8cf861ee37a06
Harry He <harry.he.zz@gmail.com>
2023-01-18 11:12:42 -0800

    changed name

--- home.js
+++ home.js
@@ -7,7 +7,7 @@ $(document).ready(function () {
         const message = $('#chatbot-input').val();
         $.ajax({
             type: 'POST',
-            url: 'http://localhost:3000/chatbot',
+            url: 'https://lab02backend.onrender.com/chatbot',
             data: {
                 message: message
             },
